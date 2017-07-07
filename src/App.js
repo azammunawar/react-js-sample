9df83c99313c8c45c0a8b94bbaf69d74259c1bae
azammunawar <azi.munawarali@gmail.com>
2017-07-07 18:16:12 +0500

    youtube videos on home page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import {Link} from 'react-router-dom';
 import SearchBar from './searchbar';
@@ -7,46 +7,49 @@ import YTSearch from 'youtube-api-search';
 import './App.css';
 
 
-
 const api_key = 'AIzaSyBxVF7VJvvLJc1mwJbJ8njfMweESosawOc';
 
 
-
 class App extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state= {input:''};
-        this.state = {videos:[2,3]};
-        const video = 12;
-        YTSearch({key:api_key, term: 'surfboards'}, (data) => {
-            console.log('data', data);
-           this.setState({videos: data});
-            console.log('videos search', this.state.videos);
-            //const video = <VideoList videos={this.state.videos} />
-        });
-        
-    }
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-            {this.state.input}
-           length: {this.state.videos.length}
-          <h2>Welcome to React</h2>
-            <input type="text" onChange={event => this.setState({input: event.target.value})}/>
-        </div>
-        <p className="App-intro">
-            <Link to="/about" >About</Link>
-          To get started, edit <code >src/App.js</code> and save to reload.
-        </p>
-          <SearchBar/>
-          <VideoList videos={this.state.videos} />
-          {this.video}
+        this.state = {input: ''};
+        this.state = {videos: []};
 
-      </div>
-    );
-  }
+            YTSearch({key: api_key, term: 'most recent'}, (data) => {
+                console.log('data', data);
+                this.setState({videos: data});
+                console.log('videos search', this.state.videos);
+                //const video = <VideoList videos={this.state.videos} />
+            });
+
+
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    {this.state.input}
+                    length: {this.state.videos.length}
+                    <h2>Welcome to React</h2>
+                    <input
+                            type="text"
+
+                    />
+                </div>
+                <p className="App-intro">
+                    <Link to="/about">About</Link>
+                    To get started, edit <code >src/App.js</code> and save to reload.
+                </p>
+                <SearchBar/>
+                <VideoList videos={this.state.videos}/>
+                {this.video}
+
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/video_list_item.js
+++ src/video_list_item.js
@@ -3,11 +3,22 @@ import React, { Component } from 'react';
 class VideoListItem extends Component {
     constructor(props){
         super(props);
-        
+
     }
     render (){
-        return(    
-             <li>Video</li>
+        const imgurl = this.props.video.snippet.thumbnails.default.url;
+        const description = this.props.video.snippet.description;
+        const title = this.props.video.snippet.title;
+        return(
+             <li className="videolist">
+                 <div className="img"><img src={imgurl} alt=""/></div>
+                 <div className="content">
+                     <div className="title">{title}</div>
+                     <div className="description">{description}</div>
+                 </div>
+
+             </li>
+
         );
     }
 }
